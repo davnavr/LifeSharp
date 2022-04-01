94e5c0703b7161589fa7a0f736b50c7420a19ebc
David N <davidcalieln@gmail.com>
2022-03-31 18:40:29 -0700

    Add implementations for Id

--- lifesharp/src/ast.rs
+++ lifesharp/src/ast.rs
@@ -1,26 +1,44 @@
 //! Contains types used to represent the structure of LifeSharp source files.
 
+use crate::identifier;
+use crate::location::{Offset, OffsetRange};
 use std::fmt::{Display, Formatter};
-use crate::location::OffsetRange;
-
-pub use crate::identifier::Id;
 
 /// Represents content in a source code file associated with its location.
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub struct Located<T> {
     /// Value representing something that was parsed.
-    pub contents: T,
+    pub content: T,
     /// Location in the source code file.
     pub location: OffsetRange,
 }
+
+impl<T> Located<T> {
+    /// Associates parsed content with a location in the source file.
+    pub fn new(content: T, start: Offset, end: Offset) -> Self {
+        Self {
+            content,
+            location: OffsetRange { start, end },
+        }
+    }
+}
+
+impl<T: Display> Display for Located<T> {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        Display::fmt(&self.content, f)
+    }
+}
+
+/// An identifier in the source code file along with its location.
+pub type Id<'i> = Located<&'i identifier::Id>;
 
 /// Represents a function definition.
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub struct FunctionDefinition<'t> {
     /// The name of the function.
-    pub name: Located<&'t Id>,
+    pub name: Id<'t>,
 }
 
 /// Represents a top-level declaration defined in a source code file.
--- lifesharp/src/identifier.rs
+++ lifesharp/src/identifier.rs
@@ -1,8 +1,91 @@
 //! Types to represent identifier strings in source code.
 
+use std::fmt::{Debug, Display, Formatter};
+
 /// Represents a borrowed identifier string.
-#[derive(Debug, Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq)]
 #[repr(transparent)]
 pub struct Id(str);
 
+/// Error used when a string is not a valid identifier.
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum InvalidError {
+    /// Used when an identifier contains an invalid character.
+    #[error("{code_point} at index {index} is not a valid identifier character")]
+    InvalidCodePoint {
+        /// The invalid code point.
+        code_point: char,
+        /// The code point index of the code point in the string.
+        index: usize,
+    },
+    /// Used when an identifier is empty.
+    #[error("identifiers must not be empty")]
+    Empty,
+}
 
+impl Id {
+    /// Creates a reference to a borrowed identifier string from a reference to a borrowed UTF-8 string.
+    ///
+    /// # Safety
+    /// Callers must ensure that the identifier string is not empty and contains valid identifier characters.
+    pub unsafe fn new_unchecked(identifier: &str) -> &Self {
+        std::mem::transmute(identifier)
+    }
+
+    /// Interprets the contents of this identifier string as a borrowed UTF-8 string.
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl<'i> TryFrom<&'i str> for &'i Id {
+    type Error = InvalidError;
+
+    fn try_from(identifier: &str) -> Result<&Id, InvalidError> {
+        if let Some((index, bad)) = identifier.chars().enumerate().find(|(i, c)| {
+            !(c.is_ascii_alphabetic() || *c == '_') || (*i == 0 && c.is_ascii_digit())
+        }) {
+            Err(InvalidError::InvalidCodePoint {
+                code_point: bad,
+                index,
+            })
+        } else {
+            unsafe {
+                // Safety: Validation is performed above.
+                Ok(Id::new_unchecked(identifier))
+            }
+        }
+    }
+}
+
+impl Debug for Id {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        Debug::fmt(&self.0, f)
+    }
+}
+
+impl Display for Id {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+impl std::ops::Deref for Id {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl std::convert::AsRef<str> for Id {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl std::borrow::Borrow<str> for Id {
+    fn borrow(&self) -> &str {
+        self.as_str()
+    }
+}
